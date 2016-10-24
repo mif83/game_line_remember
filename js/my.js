b8fe521b00c577a253bef711a61b4cf1b0555595
Dima Esipenko <mif83@mail.ru>
2016-10-24 09:08:23 +0300

    implemented algoritm generate lines without DOM

--- js/my.js
+++ js/my.js
@@ -1,6 +1,10 @@
 /**
  * Created by dima on 17.10.2016.
  */
+
+/***************
+* view
+***************/
 function createField(option){
     console.log(option);
     var option = option || {},
@@ -25,65 +29,91 @@ function createField(option){
     parent.appendChild(svg);
 
 };
+function clearLine(){
+    var p = document.getElementById(option.svgId),
+        lines = p.querySelectorAll("line");
+    for (var i = 0; i < lines.length; i++){
+        p.removeChild(lines[i]);
+    }
+    arrLines.length = 0;
+}
+function clearField(){
+    option.parentContainer.innerHTML ="";
+    arrLines.length = 0;
+}
+function showLines(arrLines){
+    var str = "",
+        circle = document.getElementById(option.svgId).querySelectorAll("circle");
+    for (var i = 0; i < arrLines.length; i++){
+        console.log("i = " + i);
+        console.log("arrLines.length = " + arrLines.length);
+        console.log("arrLines[i] = " + JSON.stringify(arrLines[i]));
+        str += "<line x1='"+ parseFloat(circle[arrLines[i].iStart].attributes.cx.value)+"' " +
+            "y1='" +parseFloat(circle[arrLines[i].iStart].attributes.cy.value)+ "'" +
+            "x2='"+ parseFloat(circle[arrLines[i].iEnd].attributes.cx.value)+"' " +
+            "y2='"+ parseFloat(circle[arrLines[i].iEnd].attributes.cy.value)+"' />";
+    }
+    document.getElementById(option.svgId).innerHTML += str;
+}
+/***************
+* model
+***************/
 function generateLine(option){
     var str = '',
-       // circle = document.getElementById(option.svgId).querySelectorAll("circle"),
         d = Date.now(),
         length = option.width*option.height,
         arrPointTargeted = [];
 
 
-  for (var i = 0; i < length; i++ ){
-      var obj ={};
-      if (arrPointTargeted[i] || i == length-1) continue;
+    for (var i = 0; i < length; i++ ){
+        var obj ={};
+        if (arrPointTargeted[i] || i == length-1) continue;
 
-      if( (i+1) % option.width == 0 || (i+1) >= option.width*(option.height - 1)){
+        if( (i+1) % option.width == 0 || (i+1) >= option.width*(option.height - 1)){
 
-          if ((i+1) % option.width == 0 ){
-              obj.iStart = i;
-              arrPointTargeted[i] = true;
-              obj.iEnd = generateVertical();
-          } else {
-              if(arrPointTargeted[i+1]){
-                  console.log("target next element");
-                  continue;
-              }
-              obj.iStart = i;
-              arrPointTargeted[i] = true;
-              obj.iEnd = generateHorizontal();
-          }
-      } else {
-          obj.iStart = i;
-          arrPointTargeted[i] = true;
-          if (choiceDirect()){
-              //vertical
-              //проверяем предпоследний ряд на правильность генерирования, мы не можем генерировать линию вниз,
-              // если внизу нечетное количество незаполненых элементов
-              if (i > (option.width*(option.height-2)-1) && i < (option.width*(option.height-1) -1) ){
-                  if (chekDirect(i, arrPointTargeted)){
-                      obj.iEnd = generateVertical();
-                  } else{
-                      obj.iEnd = generateHorizontal();
-                  }
-              //    console.log("предпоследний ряд");
-              } else {
-                  obj.iEnd = generateVertical();
-              }
+            if ((i+1) % option.width == 0 ){
+                obj.iStart = i;
+                arrPointTargeted[i] = true;
+                obj.iEnd = generateVertical();
+            } else {
+                if(arrPointTargeted[i+1]){
+                    console.log("target next element");
+                    continue;
+                }
+                obj.iStart = i;
+                arrPointTargeted[i] = true;
+                obj.iEnd = generateHorizontal();
+            }
+        } else {
+            obj.iStart = i;
+            arrPointTargeted[i] = true;
+            if (choiceDirect()){
+                //vertical
+                //проверяем предпоследний ряд на правильность генерирования, мы не можем генерировать линию вниз,
+                // если внизу нечетное количество незаполненых элементов
+                if (i > (option.width*(option.height-2)-1) && i < (option.width*(option.height-1) -1) ){
+                    if (chekDirect(i, arrPointTargeted)){
+                        obj.iEnd = generateVertical();
+                    } else{
+                        obj.iEnd = generateHorizontal();
+                    }
+                    //    console.log("предпоследний ряд");
+                } else {
+                    obj.iEnd = generateVertical();
+                }
 
-          } else {
-              //horizontal
-              if ( arrPointTargeted[i+1] == true){
-                  obj.iEnd = generateVertical();
-              } else {
-                  obj.iEnd = generateHorizontal();
-              }
-          }
-      }
-      arrLines.push(obj);
-  }
-  //  console.log((Date.now()- d));
-  //  document.getElementById(option.svgId).innerHTML += str;
-  for (i = 0; i < length; i++){
+            } else {
+                //horizontal
+                if ( arrPointTargeted[i+1] == true){
+                    obj.iEnd = generateVertical();
+                } else {
+                    obj.iEnd = generateHorizontal();
+                }
+            }
+        }
+        arrLines.push(obj);
+    }
+    for (i = 0; i < length; i++){
         if (!arrPointTargeted[i]){
             console.log("new call");
             arrPointTargeted.length = 0;
@@ -109,20 +139,6 @@ function choiceDirect(){
     //return 1 = vertical, 0 = horizontal;
     return Math.round (Math.random());
 };
-/*function isTargeted(elem){
-    return elem.target;
-}*/
-function clearLine(){
-    var p = document.getElementById(option.svgId),
-        lines = p.querySelectorAll("line");
-    for (var i = 0; i < lines.length; i++){
-        p.removeChild(lines[i]);
-    }
-
-}
-function clearField(){
-    option.parentContainer.innerHTML ="";
-}
 function chekDirect(i, arrPointTargeted){
     var count = 0,
         downElemIndex = i+option.width-1;
@@ -131,41 +147,38 @@ function chekDirect(i, arrPointTargeted){
         downElemIndex--;
     }
 
-    return count % 2 === 0 ? true : false;
+    return count % 2 === 0;
 }
-function showLines(arrLines){
-    var str = "",
-        circle = document.getElementById(option.svgId).querySelectorAll("circle");
-    for (var i = 0; i < circle.length; i++){
-        str += "<line x1='"+ parseFloat(circle[arrLines[i].iStart].attributes.cx.value)+"' " +
-            "y1='" +parseFloat(circle[arrLines[i].iStart].attributes.cy.value)+ "'" +
-             "x2='"+ parseFloat(circle[arrLines[i].iEnd].attributes.cx.value)+"' " +
-            "y2='"+ parseFloat([arrLines[i].iEnd].attributes.cy.value)+"' />";
-    }
-    document.getElementById(option.svgId).innerHTML += str;
-}
+/***************
+* controller
+***************/
+document.getElementById("clear").addEventListener("click", clearLine);
+document.getElementById("generate").addEventListener("click", function(){
+    generateLine(option);
+    showLines(arrLines);
+});
+
+document.getElementById("rows").addEventListener("change", function(e){
+    option.height = parseInt(e.target.value);
+    clearField();
+    createField(option);
+});
+document.getElementById("cols").addEventListener("change", function(e){
+    option.width = parseInt(e.target.value);
+    clearField();
+    createField(option);
+});
+
+/***************
+* data
+***************/
 
 var option = {
     width: 10,
     height: 5,
     parentContainer: document.getElementById("field"),
     svgId: "lalala"
-},
+    },
     arrLines = [];
-document.getElementById("clear").addEventListener("click", clearLine);
-document.getElementById("generate").addEventListener("click", function(){
-    generateLine(option);
-    showLines(arrLines);
-});
-/*document.getElementById("rows").addEventListener("change", function(e){
-    option.height = e.target.value;
-    clearField();
-    createField(option);
-});
-document.getElementById("cols").addEventListener("change", function(e){
-    option.width = e.target.value;
-    clearField();
-    createField(option);
-});*/
 
 createField(option);
